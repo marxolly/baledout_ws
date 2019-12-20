a15a034f2d3b1289587894ea9e59ec3a20b738c9
marxolly <mark@solly.com.au>
2019-12-20 14:39:27 +1100

    Update DashboardController.php

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -15,17 +15,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        die('index controller');
         $orders = array();
         $client_id = 0;
         $clients = array();
         $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();;
-        $variables = array(
-            'client_id'             =>  $client_id,
-            'orders'                =>  $orders,
-            'clients'               =>  $clients,
-            'user_role'             =>  $user_role
-        );
-        echo "<pre>",print_r($variables),"</pre>";die();
         Config::setJsConfig('curPage', "dashboard");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/dashboard/", Config::get('VIEWS_PATH') . 'dashboard/index.php',[
             'client_id'             =>  $client_id,
