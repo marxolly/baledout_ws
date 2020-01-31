8a360d848283b233295a5c44a705c715b25af814
marxolly <mark@solly.com.au>
2020-01-31 11:26:30 +1100

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -23,9 +23,27 @@ $c = 1;
             <h2>Header 2</h2>
             <h3>Header 3</h3>
             <h4>Header 4</h4>
+            <p>Standard paragraph of text.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod mattis felis et iaculis. Donec iaculis vehicula consequat. Curabitur ac elit vel urna molestie iaculis. Etiam tristique ligula nec commodo imperdiet. Phasellus iaculis, elit at porttitor congue, odio orci faucibus lacus, vel dapibus enim urna id sapien. Sed ultricies nulla est, quis consectetur sapien placerat ut. Morbi congue congue erat non iaculis. Nam ultrices sem sit amet interdum egestas. Etiam vel placerat tortor. Cras non pulvinar purus. Nam id feugiat dui. Quisque eleifend varius nisl, ac varius augue fringilla dapibus. Quisque quis volutpat lacus, et ultricies nisi. Aliquam erat volutpat. Sed rhoncus nibh eget nunc fringilla, vel pretium eros dictum.</p>
         </div><!-- end 1st row -->
         <div class="row"><!-- second row -->
-
+            <p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    <li>Quisque consequat tellus vitae justo pharetra ultricies.</li>
+                    <li>Mauris eget nisl dictum, tristique lorem vitae, rhoncus leo.</li>
+                    <li>Aliquam non tellus interdum, tincidunt lorem vitae, varius purus.</li>
+                    <li>Morbi eu nisi finibus, scelerisque orci non, consequat felis.</li>
+                    <li>Sed non felis a arcu vestibulum mattis quis eget metus.</li>
+                </ul>
+            </p>
+            <p>
+                <ul>
+                    <li>Morbi iaculis diam id lectus sagittis vestibulum.</li>
+                    <li>Etiam facilisis est ultrices nunc porta malesuada.</li>
+                    <li>Nunc vel diam varius mi tempus lacinia.</li>
+                </ul>
+            </p>
         </div> <!-- end 2nd row -->
         <div class="row">
 
