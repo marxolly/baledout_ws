9ae6223557641180281972e515beed3a32ad1fc0
marxolly <mark@solly.com.au>
2020-02-03 14:10:36 +1100

    Update jobStatus.php

--- app/views/sitesettings/jobStatus.php
+++ app/views/sitesettings/jobStatus.php
@@ -3,5 +3,9 @@
 ?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-
+    <div class="row">
+        <div class="col-lg-12">
+            <select  class="form-control selectpicker" name="status"><?php echo $this->controller->jobstatus->getSelectStatus();?></select>
+        </div>
+    </div>
 </div>
