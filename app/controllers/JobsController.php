ca28e9a1ad50013ee0f3c1e81ad45f366c90c0c9
marxolly <mark@solly.com.au>
2020-02-07 10:55:23 +1100

    Jobs Controller setup

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -14,41 +14,67 @@ class JobsController extends Controller{
         $this->Security->config("form", [ 'fields' => ['csrf_token']]);
     }
 
-
+    public function viewJobs()
+    {
+        //echo "<pre>",print_r($this->request->params['args']),"</pre>";die();
+        $client_name = "All Clients";
+        $courier_id = -1;
+        $client_id = 0;
+        $fulfilled = 0;
+        $state = "";
+        $ff = "Unfulfilled";
+        if(!empty($this->request->params['args']))
+        {
+            if(isset($this->request->params['args']['client']))
+            {
+                $client_id = $this->request->params['args']['client'];
+                $client_name = $this->client->getClientName($client_id);
+            }
+            if(isset($this->request->params['args']['courier']))
+            {
+                $courier_id = $this->request->params['args']['courier'];
+            }
+            if(isset($this->request->params['args']['fulfilled']))
+            {
+                $fulfilled = $this->request->params['args']['fulfilled'];
+                $ff = "Fulfilled";
+            }
+            if(isset($this->request->params['args']['state']))
+            {
+                $state = $this->request->params['args']['state'];
+            }
+        }
+        $page_title = "$ff Orders For $client_name";
+        //$orders = $this->order->getUnfulfilledOrders($client_id, $courier_id, 0);     getAllOrders($client_id, $courier_id = -1, $fulfilled = 0, $store_order = -1)
+        $orders = $this->order->getAllOrders($client_id, $courier_id, $fulfilled, 0, $state);
+        //render the page
+        Config::setJsConfig('curPage', "view-orders");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/orders/", Config::get('VIEWS_PATH') . 'orders/viewOrders.php', [
+            'page_title'    =>  $page_title,
+            'client_name'   =>  $client_name,
+            'client_id'     =>  $client_id,
+            'courier_id'    =>  $courier_id,
+            'orders'        =>  $orders,
+            'fulfilled'     =>  $fulfilled,
+            'state'         =>  $state
+        ]);
+    }
 
 
     public function isAuthorized(){
         $action = $this->request->param('action');
-        //$role = Session::getUserRole();
         $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
         $resource = "orders";
-
         //only for admin
         Permission::allow('admin', $resource, "*");
         Permission::allow('super admin', $resource, "*");
-        //warhouse users
+        //warehouse users
         Permission::allow('warehouse', $resource, array(
-            "orderDispatching",
-            "orderPacking",
-            "orderPicking",
-            "orderSearch",
-            "orderSearchResults",
-            "viewOrders",
-            "orderUpdate",
-            "addressUpdate",
-            "orderEdit",
-            "viewDetails",
-            "viewStoreorders"
+
         ));
-        //only for clients
+        //client users
         $allowed_resources = array(
-            "addOrder",
-            "addOrderTest",
-            "bookPickup",
-            "bulkUploadOrders",
-            "clientOrders",
-            "orderTracking",
-            "orderDetail",
+
         );
         Permission::allow('client', $resource, $allowed_resources);
         return Permission::check($role, $resource, $action);
