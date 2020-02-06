f2b616ded725a2ed4d98a0c3fcd198fff24ceec1
marxolly <mark@solly.com.au>
2020-02-06 14:44:31 +1100

    Update Email.php

--- app/core/Email.php
+++ app/core/Email.php
@@ -41,5 +41,31 @@
         }
     }
 
+    public static function sendPasswordReset($user_id, $name, $email, $password_token)
+    {
+        $mail = new PHPMailer();
+
+        $body = file_get_contents(Config::get('EMAIL_TEMPLATES_PATH')."passwordreset.html");
+        $replace_array = array("{LINK}", "{NAME}");
+        $replace_with_array = array(Config::get('EMAIL_PASSWORD_RESET_URL') . "?id=" . urlencode(Encryption::encryptId($user_id)) . "&token=" . urlencode($password_token), $name);
+        $body = str_replace($replace_array, $replace_with_array, $body);
+
+        $mail->SetFrom(Config::get('EMAIL_FROM'), Config::get('EMAIL_FROM_NAME'));
+
+        $mail->AddAddress($email, $name);
+
+        $mail->Subject = "Reset your password for 3PLPLUS WMS system";
+
+        $mail->AddEmbeddedImage(IMAGES."email_logo.png", "emailfoot", "email_logo.png");
+
+        $mail->MsgHTML($body);
+
+        if(!$mail->Send())
+        {
+            Logger::log("Mail Error", print_r($mail->ErrorInfo, true), __FILE__, __LINE__);
+            throw new Exception("Email couldn't be sent to ". $name);
+        }
+    }
+
  }
 
