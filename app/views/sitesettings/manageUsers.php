ef53059efca1881910cd9c9dc6e63902664d034a
marxolly <mark@solly.com.au>
2020-02-06 13:33:29 +1100

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -37,7 +37,7 @@
                         <?php $users = $this->controller->user->getAllUsersByRoleID($ur['id'], $active);
                         if(count($users)):
                             foreach($users as $user):
-                            $last_log = ($user['last_log'] > 0)? date("F j, Y, g:i a", $user['last_log']):"Never"?>
+                            $last_log = ($user['last_log'] > 0)? date("D F j, Y, g:i a", $user['last_log']):"Never"?>
                             <tr>
                                 <td class="username"><img src="/images/profile_pictures/<?php echo $user['profile_picture'];?>" alt="profile image" class="img-thumbnail" /> <?php echo $user['name'];?></td>
                                 <td><?php echo $rolename;?></td>
