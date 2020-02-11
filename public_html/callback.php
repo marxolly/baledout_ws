b0cea47fb5401872f07fe08476bcaa88b5053919
marxolly <mark@solly.com.au>
2020-02-11 12:29:11 +1100

    Update callback.php

--- public_html/callback.php
+++ public_html/callback.php
@@ -57,7 +57,7 @@ ini_set('display_errors', 'On');
 
         } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
             echo "Callback failed";
-            print_r($e);
+            echo "<pre>",print_r($e),"</pre>";
             exit();
         }
     }
