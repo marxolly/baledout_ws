2241d4a280520c756f603d32778724f0a2da3e89
marxolly <mark@solly.com.au>
2020-02-12 10:29:16 +1100

    Update calcinai_demo.php

--- public_html/calcinai_demo.php
+++ public_html/calcinai_demo.php
@@ -8,7 +8,7 @@ session_start();
 $provider = new Xero([
         'clientId'                => '62609541B59A46CAB4AC6F374ED3B0F3',
         'clientSecret'            => '4-jtbm47vaG4BmDt5oTfg4V-_z2XLN0N_gLOVldpuDQInoBR',
-        'redirectUri'             => 'https://portal.baledout.com.au/callback.php',
+        'redirectUri'             => 'https://portal.baledout.com.au/calcinai_demo.php',
         'urlAuthorize'            => 'https://login.xero.com/identity/connect/authorize',
         'urlAccessToken'          => 'https://identity.xero.com/connect/token',
         'urlResourceOwnerDetails' => 'https://api.xero.com/api.xro/2.0/Organisation'
