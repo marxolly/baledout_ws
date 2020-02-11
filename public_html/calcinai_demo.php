11315fd80ac4fb05ce0db9c9ac95965771aa22bf
marxolly <mark@solly.com.au>
2020-02-12 10:28:00 +1100

    Update calcinai_demo.php

--- public_html/calcinai_demo.php
+++ public_html/calcinai_demo.php
@@ -6,17 +6,20 @@ include __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 $provider = new Xero([
-    'clientId' => '62609541B59A46CAB4AC6F374ED3B0F3',
-    'clientSecret' => '4-jtbm47vaG4BmDt5oTfg4V-_z2XLN0N_gLOVldpuDQInoBR',
-    'redirectUri' => 'https://portal.baledout.com.au/calcinai_demo.php',
-]);
+        'clientId'                => '62609541B59A46CAB4AC6F374ED3B0F3',
+        'clientSecret'            => '4-jtbm47vaG4BmDt5oTfg4V-_z2XLN0N_gLOVldpuDQInoBR',
+        'redirectUri'             => 'https://portal.baledout.com.au/callback.php',
+        'urlAuthorize'            => 'https://login.xero.com/identity/connect/authorize',
+        'urlAccessToken'          => 'https://identity.xero.com/connect/token',
+        'urlResourceOwnerDetails' => 'https://api.xero.com/api.xro/2.0/Organisation'
+    ]);
 
 if (!isset($_GET['code'])) {
-
     // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl([
-        'scope' => ['openid email profile accounting.settings accounting.transactions']
-    ]);
+    $options = [
+            'scope' => ['openid email profile offline_access accounting.settings accounting.transactions accounting.contacts accounting.journals.read accounting.reports.read accounting.attachments']
+    ];
+    $authUrl = $provider->getAuthorizationUrl($options);
 
 
 
