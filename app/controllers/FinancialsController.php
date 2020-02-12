9f2b0dec3cae7a6f6ee09743eba25111903c63ad
marxolly <mark@solly.com.au>
2020-02-12 15:52:45 +1100

    xero in fanaces

---
+++ app/controllers/FinancialsController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Financials controller
+ *
+
+ * @author     Mark Solly <mark@baledout.com.au>
+ */
+
+class FinacialsController extends Controller
+{
+    public function xeroInfo()
+    {
+        $org = $this->xero_auth->$org;
+
+        Config::setJsConfig('curPage', "xero-info");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/xero-info.php',[
+            'org'        => $org,
+            'page_title' => 'Xero Info'
+        ]);
+    }
+
+    public function isAuthorized(){
+        return true;
+    }
+}
---
+++ app/views/financials/xero-info.php
@@ -0,0 +1,11 @@
+<?php
+  $line = $c = 1;
+?>
+<div id="page-wrapper">
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div class="row">
+        <div class="col-md-12">
+            <?php echo "ORG<pre>",print_r($org),"</pre>"; ?> 
+        </div>
+    </div>
+</div>
