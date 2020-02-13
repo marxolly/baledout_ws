4d47ac511dbe448ee3fedd95e93cdfb443088587
marxolly <mark@solly.com.au>
2020-02-13 13:28:26 +1100

    Update xero-info.php

--- app/views/financials/xero-info.php
+++ app/views/financials/xero-info.php
@@ -5,7 +5,7 @@
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div class="row">
         <div class="col-md-12">
-            <?php echo "ORG<pre>",print_r($contacts),"</pre>"; ?> 
+            <?php echo "ORG<pre>",print_r($org),"</pre>"; ?> 
         </div>
     </div>
 </div>
