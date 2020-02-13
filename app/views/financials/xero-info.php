157e1c8eaa817ade13e56ccfc69c4ed621d7abab
marxolly <mark@solly.com.au>
2020-02-13 13:45:26 +1100

    Update xero-info.php

--- app/views/financials/xero-info.php
+++ app/views/financials/xero-info.php
@@ -5,7 +5,16 @@
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div class="row">
         <div class="col-md-12">
-            <?php echo "ORG<pre>",print_r($org),"</pre>"; ?> 
+            <?php
+            foreach($contacts as $contact)
+            {
+                //do something here
+
+                //this will give you the raw data without the object structure
+                echo "<pre>",print_r($contact->toStringArray()),"</pre>";
+            }
+
+            ?>
         </div>
     </div>
 </div>
