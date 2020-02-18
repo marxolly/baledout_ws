de7af2cd124f11a9bf492408da085d436b808070
marxolly <mark@solly.com.au>
2020-02-18 11:20:57 +1100

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -46,6 +46,19 @@ $c = 1;
             </p>
         </div> <!-- end 2nd row -->
         <div class="row">
+            <h2>Buttons</h2>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-light">Light</button>
+            <button type="button" class="btn btn-dark">Dark</button>
+
+            <button type="button" class="btn btn-link">Link</button>
+        </div>
+        <div class="row">
             <table width="100%" class="table-striped table-hover" id="the_table" style="width:100%">
                 <thead>
                     <tr>
